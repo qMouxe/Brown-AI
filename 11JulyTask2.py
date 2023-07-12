1d15d8d1f00bafe02e4b71389c45d2583e61352d
qMouxe <seymour3301@gmail.com>
2023-07-12 08:45:44 -0400

    Create

---
+++ 11JulyTask2.py
@@ -0,0 +1,15 @@
+unit = str(input("Input unit: minutes = m, hours = h"))
+num = int(input("Any number"))
+temp = 0
+if unit == "h":
+    temp = num
+    num = num*60
+    print(str(temp) +  " hours is equal to " + str(num) + " minutes")
+elif unit == "m":
+    temp = num
+    num = num/60
+    print(str(temp) + " minutes is equal to " + str(num) + " hours")
+
+    
+
+    
---
+++ 11JulyTask3.py
@@ -0,0 +1,2 @@
+word = str(input("Input any word: "))
+print("There are " + str(len(word)) + " letters in the word " + "'" + word + "'")
